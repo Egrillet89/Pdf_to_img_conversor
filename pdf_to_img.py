5742bac0337567aa3df69277a766d777b63ea15b
Egrillet89 <egrillet.valera@gmail.com>
2024-03-03 17:20:47 -0400

    F:modificacion script y add poopler file

--- pdf_to_img.py
+++ pdf_to_img.py
@@ -1,12 +1,12 @@
 from pdf2image import convert_from_path
 import os
 
-poppler_path = r"D:\\Descargas\\poppler-0.68.0\\poppler-0.68.0\\bin"
-pdf_patch = r'D:\\Documentos\\alexys\\CERTIFICADOS Y CURSOS ALEXYS.pdf'
+poppler_path = r"RUTA DE INSTALACION DE POOPLER"
+pdf_patch = r'RUTA DE ARCHIVO PDF'
 
 pages = convert_from_path(pdf_path= pdf_patch, poppler_path= poppler_path)
 
-saving_folder = r'C:\\Users\\Eleazar Grillet\\Desktop\\Converted_pdf'
+saving_folder = r'RUTA DONDE DESEA GUARDAR IMG CONVERTIDA'
 
 c = 1
 
